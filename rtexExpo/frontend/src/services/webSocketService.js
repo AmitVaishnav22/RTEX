3312403a1a1ce456831e98bc49ca56cf1d0ba745
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-09-15 00:12:40 +0530

    Final Fix of working Dashboard

--- rtexExpo/frontend/src/services/webSocketService.js
+++ rtexExpo/frontend/src/services/webSocketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://rtex-1.onrender.com.", {
+const socket = io("https://rtex-1.onrender.com", {
   transports: ["websocket"],
 });
 
